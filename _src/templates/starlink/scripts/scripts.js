a016b62ee3c0f5f5f8518ea593d8bd8b27bb850d
mao <green.turban@gmail.com>
2017-02-28 15:01:34 +0200

    starlink: scripts combine in template

--- _src/templates/starlink/scripts/scripts.js
+++ _src/templates/starlink/scripts/scripts.js
@@ -1,28 +1,5 @@
 jQuery(document).ready(function ($) {
 
-  /****** Sticky menu on /services pages ******/
-
-  // // TODO remove comments after checking
-  // jQuery(window).scroll(function(){
-  //     var headerAndTopBlHeight = jQuery("header").innerHeight() /*+ jQuery(".servicesBgBl1Margin").innerHeight()*/;
-  //     if (jQuery(".secondaryConsultingMenuBl").length > 0) {    // TODO move this into 2nd .sticky addition instruction
-  //         if(jQuery(window).scrollTop() > headerAndTopBlHeight) {
-  //           /*jQuery('.scrollFixedConsultingBl').css('position', 'fixed');
-  //           jQuery('body').css('margin-top', '200px');
-  //           jQuery('.scrollBl').slideDown();*/
-  //           jQuery('body>div.custom-services-image-header').addClass('sticky');
-  //           jQuery('.scrollFixedConsultingBl').addClass('sticky');
-  //         } else {
-  //           jQuery('body>div.custom-services-image-header').removeClass('sticky');
-  //           jQuery('.scrollFixedConsultingBl').removeClass('sticky');
-  //           /*jQuery('.scrollFixedConsultingBl').css('position', 'relative');
-  //           jQuery('body').css('margin-top', '0');
-  ////           jQuery('.scrollBl').hide();*/
-  //         }
-  //     }
-  // });
-
-
   /*********   CONTACT FORMS   **********/
 
   // Add required attribute for input fields Fox with class='foxContactAddRequired'
@@ -70,5 +47,63 @@ jQuery(document).ready(function ($) {
             },
             'slow');
   });
+
+
+  /******** Menu utilities ********/
+
+  // Menu for screen width <767 (mobile):
+  // Expand level 1 menu items with submenus instead of following the level 1 menu link
+  jQuery("#mainmenu > li.deeper > a").click(function (e) {
+    if (jQuery(window).width() <= 767) {
+      e.preventDefault();
+      jQuery(this).siblings('ul').toggle(250, "swing", true);                               // show or hide submeny of currently clicked item
+      jQuery(this).parent().siblings("li.deeper").children("ul").hide(250, "swing", true);  // hide sibling submenus in any case
+      jQuery.dequeue();
+      jQuery(this).parent().toggleClass("expanded");
+    }
+  });
+
+  // Add hover style to parent menu item in main menu
+  jQuery("#mainmenu > li > ul").hover(
+    function () {
+      if (jQuery(window).width() > 767) {
+        jQuery(this).parent().addClass("expanded");
+      }
+    },
+    function () {
+      if (jQuery(window).width() > 767) {
+        jQuery(this).parent().removeClass("expanded");
+      }
+    }
+  );
+
+  // Hide expanded submenus when screen becomes wider than mobile
+  jQuery(window).resize(function () {
+    if (jQuery('.container').width() >= 720 ) {
+      jQuery("#mainmenu > li.deeper > ul").hide();
+      jQuery("#mainmenu > li.deeper > ul").removeAttr("style");
+    }
+  });
+
+
+
+  /*********** SEARCH BUTTON ***********/
+
+  // Search button changes
+  jQuery('.searchButton').click(function () {
+    jQuery('.mainMenuDiv').fadeOut(100);
+    setTimeout(function () {
+      jQuery('.searchLineDiv').fadeIn(200);
+      jQuery('#mod-search-searchword').focus();
+    }, 250);
+  });
+  jQuery('#mod-search-searchword').focusout(function () {
+    jQuery('.searchLineDiv').fadeOut(100);
+    setTimeout(function () {
+      jQuery('.mainMenuDiv').fadeIn(100);
+    }, 150);
+  });
+
+
 
 });
